1b5d2376218e1607df49bfaeeeaab6b67469bcdb
diegoshe <belov-93201093@mail.ru>
2019-09-21 19:50:23 +0300

    revision singnup

--- src/signupSlizh/UI/signup.jsx
+++ src/signupSlizh/UI/signup.jsx
@@ -1,15 +1,23 @@
 import  React from 'react'
 
+
+
+
 let Signup = () => {
-	return(
+	return (
 		<div>
-			<div>Sign Up <span>масленок</span> </div>
-			<div><input type="text"/></div>
-			<div><input type="password"/></div>
-			<div><input type="password"/></div>
-			<div><button>Sign Up</button></div>
+			<div><span>Id приходит сюда</span></div>
+			<div><span>ВВожу сюда Id</span><input type="text" placeholder="Login"/></div>
+			<button>Кнопка</button>
+
+			<div>Sign Up <span>масленок</span></div>
+			<div><input type="text" placeholder="Login"/></div>
+			<div><input type="password" placeholder="Password"/></div>
+			<div><input type="password" placeholder="Repeat Password"/></div>
+			<div>
+				<button>Sign Up</button>
+			</div>
 		</div>
 	)
 }
-
 export default Signup;
