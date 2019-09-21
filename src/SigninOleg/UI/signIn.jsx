0ae6b756d079296940ecf520686b5dd5ca92262f
kasperoleg <kasper_1969>
2019-09-21 19:15:56 +0300

    add s

--- src/SigninOleg/UI/signIn.jsx
+++ src/SigninOleg/UI/signIn.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 let signIn = (props) => {
 
-  return (<div>
-    <span></span>
-    <input type="text"/>
-    <button></button>
-    <button></button>
-    <button></button>
-    <span></span>
-  </div>)
+  return (
+      <div>
+        <div>signIn <span>Cards</span> </div>
+        <div><input type="text"/></div>
+        <div><input type="password"/></div>
+        <div><input type="password"/></div>
+        <div><button>signIn</button></div>
+      </div>
 }
 
 export default signIn
