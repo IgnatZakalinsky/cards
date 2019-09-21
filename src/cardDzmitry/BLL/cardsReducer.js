341cc92b152c2ef633b1a140958fc1ce7dd8f8f6
dimansolo <dimansolo84@gmail.com>
2019-09-21 18:09:56 +0300

    add  cards Reducer and add folder UI and moved cards component to UI

---
+++ src/cardDzmitry/BLL/cardsReducer.js
@@ -0,0 +1,22 @@
+let initialstate = {
+
+
+
+};
+
+
+const carsReducer = (state = initialstate, action) => {
+
+
+    switch (action.type) {
+
+
+
+
+        default:
+            return state;
+    }
+
+};
+
+export default carsReducer
