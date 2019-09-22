c43bad8470e9eb6b970ff902ea8edd82fd278648
dimansolo <dimansolo84@gmail.com>
2019-09-22 22:39:19 +0300

    added logic in the cardReducer. created action creators, component card wrapped connect, added selectors

--- src/cardDzmitry/UI/card.jsx
+++ src/cardDzmitry/UI/card.jsx
@@ -1,21 +1,40 @@
 import React from 'react';
 import {NavLink} from "react-router-dom";
+import {connect} from "react-redux";
+import {setDeckIdSuccess, setUserIdSuccess} from "../BLL/cardsReducer";
+import {getDeckId, getUserId} from "../Selectors/selectorsCard";
 
-const Card = () => {
+const Card = ({userId, deckId, setUserIdSuccess, setDeckIdSuccess}) => {
+
+
+    const userIdChange = (e) => {
+        let user = e.currentTarget.value
+        setUserIdSuccess(user)
+    };
+
+    const deckIdChange = (e) => {
+        let deck = e.currentTarget.value
+        setDeckIdSuccess(deck)
+    };
+
+    const alertUserIAndDEckId = () => {
+        console.log(userId,deckId )
+    }
+
     return (
         <div>
 
             <div>
-                <input type="text" placeholder={"userId"}/>
+                <input type="text" placeholder={"userId"} onChange={userIdChange} />
             </div>
 
             <div>
-                <input type="text" placeholder={"cardsId"}/>
+                <input type="text" placeholder={"deckId"} onChange={deckIdChange}/>
             </div>
 
 
             <div>
-                <button>set</button>
+                <button onClick={alertUserIAndDEckId}>set</button>
             </div>
 
             <div>
@@ -58,5 +77,10 @@ const Card = () => {
     )
 }
 
+const mapStateToProps = (state) => ({
+    userId: getUserId(state),
+    deckId: getDeckId(state),
+})
 
-export default Card
+
+export default connect(mapStateToProps, {setUserIdSuccess, setDeckIdSuccess})(Card)
