f8217a19ea1ed7f4cc852f87ee2e8c5dbb1eab48
kasperoleg <kasper_1969>
2019-09-22 08:51:09 +0300

    сделал по аналогии с signUp

--- src/SigninOleg/BLL/signinReduser.js
+++ src/SigninOleg/BLL/signinReduser.js
@@ -1,22 +1,37 @@
-let initialstate = {
+import {userAPI} from "../api/api";
+const PUT_ID = 'PUT_ID';
 
-
-
+let initialState = {
+    id: 1,
+    loginId: 'admin',
+    passwordId: 'password',
+    isSuccess: true
 };
 
-
-const signinReducer = (state = initialstate, action) => {
-
+const singinReducer = (state = initialState, action) => {
 
     switch (action.type) {
-
-
-
-
+        case  PUT_ID: {
+            return  {
+                ...state,
+                id: action.id
+            };
+        }
         default:
             return state;
-    }
 
+    }
 };
 
-export default signinReducer
+export const putStatus = (id) => ({type: PUT_ID, id});
+
+/*export const updateStatus = (status) => (dispatch) => {
+	userAPI.signup(status)
+		.then(response => {
+			if (response.data.resultCode === 0){
+				dispatch(putStatus(status));
+			}
+		})
+};*/
+
+export default singinReducer;
--- src/SigninOleg/UI/signIn.jsx
+++ src/SigninOleg/UI/signIn.jsx
@@ -1,15 +1,36 @@
-import React from 'react'
+import  React from 'react'
+import {connect} from "react-redux";
+import {putStatus} from "../BLL/signinReduser";
+
+
+
 
 let SignIn = (props) => {
 
-  return (
-      <div>
-        <div>SignIn <span>Cards</span> </div>
-        <div><input type="text"/></div>
-        <div><input type="password"/></div>
-        <div><button>SignIn</button></div>
-      </div>
-  );
+    let putId = (id) => {
+        props.putStatus(id)
+    }
+
+    return (
+        <div>
+            <div><span>Id приходит сюда:</span></div>
+            <div><span>Ввожу сюда Id: </span><input type="text" placeholder="ID"/></div>
+            <button onClick={putId}>Кнопка</button>
+
+            <div>Sign Ip <span>масленок</span></div>
+            <div><input type="text" placeholder="Login"/></div>
+            <div><input type="password" placeholder="Password"/></div>
+            <div>
+                <button>Sign In</button>
+            </div>
+        </div>
+    )
 }
 
-export default SignIn
+let mstp =(state)=>{
+    return {
+        id: state.id
+    }
+}
+
+export default connect(mstp, {putStatus}) (Signin);
